1e58fe3e1ba3d1f1b7d9c34ecd5264702d38c5b6
laura-furtado <laura.application15@gmail.com>
2024-11-03 18:05:06 -0300

    chore: modificações na organização e atualizações de responsividade

--- homepage/home.js
+++ homepage/home.js
@@ -1,29 +1,11 @@
 // Simulação de saldo e viagens restantes
 let saldoAtual = 4.40;
-let viagensRestantes = 1;
 
-// Função para gerar QR Code
-let qrcode = new QRCode(document.getElementById("qrcode"), {
-    width: 200,
-    height: 200
-});
-
-function generateQRCode() {
-    let dynamicData = `QR Code - Saldo: R$${saldoAtual.toFixed(2)}, Viagens: ${viagensRestantes}`;
-    qrcode.makeCode(dynamicData); // Atualiza o QR Code com dados dinâmicos
-}
-
-document.getElementById("generate-qrcode-btn").addEventListener("click", generateQRCode);
 
 // Função para recarregar saldo (simulada)
 function recarregarSaldo() {
-    // Simula uma recarga de saldo
-    let saldoElement = document.getElementById('saldo');
-    let currentSaldo = parseFloat(saldoElement.innerText.replace('R$', '').replace(',', '.'));
-    let newSaldo = currentSaldo + 5;  // Adiciona R$ 5 ao saldo
-    saldoElement.innerText = 'R$ ' + newSaldo.toFixed(2).replace('.', ',');
-
-    // Exibe o mapa e as linhas quando o saldo for atualizado
-    document.getElementById('mapa').style.display = 'block';
-    document.getElementById('linhas').style.display = 'block';
+    saldoAtual += 4.40;
+    document.getElementById("saldo-valor").innerText = `R$${saldoAtual.toFixed(1)}`;
 }
+
+document.getElementById("recarregar-saldo-btn").addEventListener("click", recarregarSaldo);
